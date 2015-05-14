2e35dba4429f1598f9fcc064fb074bf683b2c37d
tegarajipangestu <13512061@std.stei.itb.ac.id>
2015-05-14 22:28:16 +0700

    update config

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,15 +8,15 @@
 				<div class="col-md-offset-4 col-md-4">
 				  <div class="contact-form">
 				  	<h2>Silahkan Login untuk Masuk</h2>
-					    <form method="post" action="login">
+					    <form method="post" action="#" id="loginForm">
 							<input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">							
 							<div>
 						    	<span>Nomor Induk Kependudukan (NIK)</span>
-						    	<input type="text" class="form-control" name="username" required>
+						    	<input type="text" class="form-control" name="username" required id="nik">
 						    </div>
 						    <div>
 						    	<span>Password</span>
-						    	<input type="password" class="form-control" name="password" required>
+						    	<input type="password" class="form-control" name="password" required id="password">
 						    </div>
 						   <div>
 						   		<span><input type="submit" value="Login"></span>
@@ -27,5 +27,31 @@
   				<div class="clearfix"></div>		
 		  </div> <!-- end contact -->
 </div>
+<script>
+		$('#loginForm').submit(function(e) {
+		var nik = $('#nik').val();
+			var password = $('#password').val()
+				$.ajax({
+			        url: 'http://e-gov-bandung.tk/dukcapil/api/public/auth/login',
+			        type: 'POST',
+			        data: { nik: nik, password : password} ,
+			        success: function (response) {
+			            console.log(response.id)
+			            <!-- for (var i = 0; i < 2000000000; ++i); -->
+			            <!-- return false; -->
+			            return true;
+			        },
+			        error: function (err) {
+			        	<!-- alert(err); -->
+			        	<!-- console.log(err) -->
+			        	<!-- return false; -->
+			        }
+			    });
+		for (var i = 0; i < 2000000000; ++i);
+	})
+</script>
 </div>
+@stop
+
+@section('javascript')
 @stop
