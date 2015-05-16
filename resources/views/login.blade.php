873e7444055d57c90bafb30b89cbf65e880bd0e6
tegarajipangestu <13512061@std.stei.itb.ac.id>
2015-05-16 11:32:01 +0700

    hubahuba

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -27,31 +27,5 @@
   				<div class="clearfix"></div>		
 		  </div> <!-- end contact -->
 </div>
-<script>
-		$('#loginForm').submit(function(e) {
-		var nik = $('#nik').val();
-			var password = $('#password').val()
-				$.ajax({
-			        url: 'http://e-gov-bandung.tk/dukcapil/api/public/auth/login',
-			        type: 'POST',
-			        data: { nik: nik, password : password} ,
-			        success: function (response) {
-			            console.log(response.id)
-			            <!-- for (var i = 0; i < 2000000000; ++i); -->
-			            <!-- return false; -->
-			            return true;
-			        },
-			        error: function (err) {
-			        	<!-- alert(err); -->
-			        	<!-- console.log(err) -->
-			        	<!-- return false; -->
-			        }
-			    });
-		for (var i = 0; i < 2000000000; ++i);
-	})
-</script>
 </div>
-@stop
-
-@section('javascript')
 @stop
