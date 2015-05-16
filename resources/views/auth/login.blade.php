d30b6980fae71b1d01ec8a70c5acad53792996a5
tegarajipangestu <13512061@std.stei.itb.ac.id>
2015-05-16 14:50:19 +0700

    hubahuba

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,61 +1,55 @@
 @extends('app')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Login</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="remember"> Remember Me
-									</label>
-								</div>
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">Login</button>
-
-								<a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
-							</div>
-						</div>
-					</form>
+<div class="main"><!-- start main -->
+<div class="container">
+			<div class="row contact"><!-- start contact -->				
+				<div class="col-md-offset-4 col-md-4">
+				  <div class="contact-form">
+				  	<h2>Silahkan Login untuk Masuk</h2>
+					    <form method="post" action="{{url('login')}}" id="loginForm">
+							<input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">							
+							<div>
+						    	<span>Nomor Induk Kependudukan (NIK)</span>
+						    	<input type="text" class="form-control" name="nik" required id="nik" placeholder="NIK">
+						    </div>
+						    <div>
+						    	<span>Password</span>
+						    	<input type="password" class="form-control" name="password" required id="password" placeholder="Password">
+						    </div>
+						   <div>
+						   		<span><input type="submit" value="Login"></span>
+						  </div>
+					    </form>
+				    </div>
 				</div>
-			</div>
-		</div>
-	</div>
+  				<div class="clearfix"></div>		
+		  </div> <!-- end contact -->
 </div>
-@endsection
+</div>
+
+<script>
+	$('#loginForm').submit(function(e) {
+    var nik = $('#nik').val();
+    var password = $('#password').val()
+    $.ajax({
+        url: 'http://e-gov-bandung.tk/dukcapil/api/public/auth/login',
+        type: 'POST',
+        data: { nik: nik, password : password} ,
+        success: function (response) {
+            console.log(response.id)
+            // <!-- for (var i = 0; i < 2000000000; ++i); -->
+            // <!-- return false; -->
+            return true;
+            },
+            error: function (err) {
+            // <!-- alert(err); -->
+            // <!-- console.log(err) -->
+            // <!-- return false; -->
+            }
+        });
+        for (var i = 0; i < 2000000000; ++i);
+    })
+</script>
+@stop
